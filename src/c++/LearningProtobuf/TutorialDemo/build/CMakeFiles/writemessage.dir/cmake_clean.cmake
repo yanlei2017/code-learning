08432ae6d8bab7b11bef0d4f613c5c8dd8d84c26
yanlei <yanlei@zerozero.cn>
2019-06-17 13:53:53 +0800

    add protobuf tutorial demo

---
+++ src/c++/LearningProtobuf/TutorialDemo/build/CMakeFiles/writemessage.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/writemessage.dir/writemessage.cpp.o"
+  "CMakeFiles/writemessage.dir/protobuf_gen/person.pb.cc.o"
+  "../writemessage.pdb"
+  "../writemessage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/writemessage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
