45cbcef2307c1246f326e7caa5052b822a4d7ece
yanlei <yanlei6666@gmail.com>
2019-09-07 21:58:24 +0800

    change dir

--- Practice/10.cpp
+++ Practice/10.cpp
@@ -1,40 +1,64 @@
-/*题目描述
-编写一个函数，计算字符串中含有的不同字符的个数。字符在ACSII码范围内(0~127)。不在范围内的不作统计。
+package com.hp.algorithm.leaststep;
 
-输入描述:
-输入N个字符，字符在ACSII码范围内。
+import java.util.HashMap;
+import java.util.Map;
 
-输出描述:
-输出范围在(0~127)字符的个数。
-
-示例1
-输入
-复制
-abc
-输出
-复制
-3
-*/
-
-#include <iostream>
-#include <string>
-#include <algorithm>
-using namespace std;
-int main(int argc, char const *argv[])
+public class LeastSteps2Last
 {
-    string s;
-    string a;
-    cin >> s;
 
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (0 < s[i] && s[i] < 127)
-        {
-            a.push_back(s[i]);
-        }
+public static int[] leastSteps2Last(int[] input){
+    if(input.length == 1){
+        return input;
     }
-    sort(a.begin(), a.end(), less<char>());
-    a.erase(unique(a.begin(), a.end()), a.end());
-    cout << a.size();
-    return 0;
+    
+    //对于1~input[0]的每种步数i，如果走i步后①超过终点则break；②刚到终点则记录路径并return；③没到终点，则基于走i补后的字数组继续调用本方法递归寻找路径
+    //每次循环可以返回一个路径及其长度（超出长度的那次循环可能不会返回）再在返回的这些路径中取最小长度的路径,貌似用list就行哦，傻了
+    Map<Integer, int[]> subSteps = new HashMap<Integer, int[]>();
+    for(int i = 1; i <= input[0]; i++){
+        //如果走i步还没到列表结尾，则再次调用走i步后的子列表继续走
+        if(1 + i < input.length){
+            int[] nextSubList = new int[input.length - i];
+            //src:源数组  srcPos:源数组要复制的起始位置  dest:目的数组  destPos:目的数组放置的起始位置  length:要复制的长度
+            System.arraycopy(input, i, nextSubList, 0, nextSubList.length);
+            //基于走i步后的子数组再次调用本方法计算步数并返回
+            int[] nextSteps = leastSteps2Last(nextSubList);
+            
+            //当前走i步的路径currSteps=input[0] + nextSteps
+            int[] currSteps = new int[nextSteps.length + 1];
+            currSteps[0] = input[0];
+            System.arraycopy(nextSteps, 0, currSteps, 1, nextSteps.length);
+            subSteps.put(i, currSteps);
+        //如果走i步刚好到结尾，则直接将这个路径加入到map里以作最短路径对比
+        }else if(1 + i == input.length){
+            return new int[]{input[0],input[input.length-1]};
+        }
+        //如果走i步超出长度了，则不再尝试更大的步数跨度
+        else if(1 + i > input.length){
+            break;
+        }
+    };
+    
+    int[] leastSteps = null;
+    int stepNum = Integer.MAX_VALUE;
+    for(Map.Entry<Integer, int[]> entry : subSteps.entrySet()){
+        if(entry.getValue().length < stepNum){
+            leastSteps = entry.getValue(); 
+            stepNum = entry.getValue().length;
+        };
+    }
+    return leastSteps;
 }
+ 
+/**
+ * @param args
+ */
+public static void main(String[] args)
+{
+    int[] case1 = new int[]{3,4,2,1,3,1};
+    int[] case2 = new int[]{3};
+    int[] case3 = new int[]{2,2,1};
+    int[] case4 = new int[]{2,2,3,1};
+    int[] steps = leastSteps2Last(case4);
+    for(int i : steps){
+        System.out.println(i);
+    }}
---
+++ Practice/hhhh.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+vector<vector<int>> zuhe(vector<int> in, int target)
+{ //target 是希望选择M个作组合,in是选择的范围，长度为N
+    vector<vector<int>> output;
+    for (int i = 0; i < pow(2, in.size()); i++)
+    {
+        int temp = 0, count = 0;
+        vector<int> weishu;
+        for (int j = 0; j < in.size(); j++)
+        {
+            if ((i & (1 << j)) != 0)
+            {
+                weishu.push_back(j);
+                count++;
+            } //找出二进制为1的位数以及它们的位置
+        }
+        if (count == target)
+        { //位数等于M
+            vector<int> one_case;
+            for (int j = 0; j < count; j++)
+            {
+                temp = in.size() - 1 - weishu[j];
+                one_case.push_back(in[temp]);
+            }
+            output.push_back(one_case);
+        }
+    }
+    return output;
+}
+int main()
+{
+    vector<vector<int>> output;
+    int n;
+    int m;
+    cin >> n >> m;
+    vector<int> hh;
+    for (int i = 1; i <= n; i++)
+    {
+        hh.push_back(i);
+        /* code */
+    }
+
+    // vector<int> in = {1, 2, 3, 4};
+    output = zuhe(hh, m);
+    for (auto i : output)
+    {
+        for (auto j : i)
+            cout << j << ' ';
+        cout << endl;
+    }
+    // int size = output.size();
+    // int res = size % 1000000007;
+    // cout << res << endl;
+    return 0;
+}
