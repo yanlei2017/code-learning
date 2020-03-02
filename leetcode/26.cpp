b649cf4d43be2e571ceea331463050bee201b885
yanlei <1028452050@qqcom>
2020-03-02 22:56:46 +0800

    leetcode 26 已排序数组原地去除重复元素

--- leetcode/26.cpp
+++ leetcode/26.cpp
@@ -2,9 +2,13 @@
 
 //快慢指针方法
 /*
-数组完成排序后，我们可以放置两个指针 ii 和 jj，其中 ii 是慢指针，而 jj 是快指针。只要 nums[i] = nums[j]nums[i]=nums[j]，我们就增加 jj 以跳过重复项。
+数组完成排序后，我们可以放置两个指针 ii 和 jj，其中 ii 是慢指针，而 jj
+是快指针。只要 nums[i] = nums[j]nums[i]=nums[j]，我们就增加 jj 以跳过重复项。
 
-当我们遇到 nums[j] ！= nums[i] 时，跳过重复项的运行已经结束，因此我们必须把它（nums[j]）的值复制到 nums[i+1]。然后递增 i，接着我们将再次重复相同的过程，直到 jj 到达数组的末尾为止。
+当我们遇到 nums[j] ！= nums[i]
+时，跳过重复项的运行已经结束，因此我们必须把它（nums[j]）的值复制到
+nums[i+1]。然后递增 i，接着我们将再次重复相同的过程，直到 jj
+到达数组的末尾为止。
 
 作者：LeetCode
 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/solution/shan-chu-pai-xu-shu-zu-zhong-de-zhong-fu-xiang-by-/
@@ -21,15 +25,16 @@ class Solution {
     int fast = 0;
     int slow = 0;
     while (fast < nums.size()) {
-      if (nums[slow] == nums[fast]) {
-        fast++;
-      } else {
+      if (nums[fast] != nums[slow]) {
+        /*这个判断是为了避免 [1 2 3] 这种情况，会有原地复制现象*/
+        if (fast - slow > 1) {
+          nums[slow + 1] = nums[fast];
+        }
         slow++;
-        nums[slow] = nums[fast];
-        fast++;
       }
+      fast++;
     }
-    return slow + 1; /*慢指针之前都是排好序的*/ 
+    return slow + 1; /*慢指针之前都是排好序的*/
   }
 };
 
